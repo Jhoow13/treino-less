14e72a820084d8854c864020fb010418d1e48915
Joel Silva <joelcsilva@outlook.com>
2017-03-04 02:24:14 -0300

    commit inicial

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var less = require('gulp-less');
+
+
+gulp.task('clean', function () {
+    return gulp.src('css/*')
+        .pipe(clean());
+});
+
+gulp.task('less', function () {
+    return gulp.src('./less/**/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('less/**/*.less', ['less']);
+});
+
+gulp.task('default', ['watch']);
